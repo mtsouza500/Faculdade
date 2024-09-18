d4a945dc8dc4410b3b439a846b16f1c248c8d331
Matheus <mtsouzamarins500@hotmail.com>
2024-09-17 21:48:27 -0300

    Add files via upload

---
+++ algoritmos7.py
@@ -0,0 +1,30 @@
+# def busca(lista, chave):
+
+# lista = [10,11,12,13]
+# for x,y in enumerate(lista):
+#     print(x,y)
+    
+
+# lista = [10,11,12,13,14]
+# chave = 12
+
+# def buscaBinária(lista, chave): 
+#     pos_ini = 0
+#     pos_fim = len(lista) - 1
+#     while pos_ini <= pos_fim:
+#         pos_meio = (pos_ini + pos_fim) // 2
+#     if lista[pos_meio] == chave:
+#         return pos_meio
+#     print(f"A chave é: {chave}, e está corretamente na lista")
+#     if lista[pos_meio] > chave: 
+#         pos_fim = pos_meio - 1
+#         print(f"A chave é: {chave}, e é maior do que o meio {pos_meio}")
+#     if lista[pos_meio] < chave: 
+#         pos_ini = pos_meio + 1
+#         print(f"A chave é: {chave}, e é menor que o meio {pos_meio}")
+#     return -1
+
+# import math
+# n=10000000
+# res= math.log2(n)
+# print(res)
